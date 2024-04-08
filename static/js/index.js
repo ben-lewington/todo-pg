5eed54c6840d53b20f6e84b49d40fc57dc2d36fa
Ben Lewington <ben.lewington1991@gmail.com>
2024-04-08 23:23:44 +0100

    Added sortable as a vendored dependency, renamed thirdparty -> vendor

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,6 @@
+import { Sortable } from './vendor/sortable.esm.js'
 
+document.querySelectorAll("[data-todo-list]")
+    .forEach((el) => {
+        var sortableInst = Sortable.create(el, {});
+    })
